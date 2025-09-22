0833b05a339332cef0558831ca6eafb1f4e634f4
isaac <isacmico50@gmail.com>
2025-09-21 20:15:41 -0400

    fix: subir título 5px más para mejor alineación con logo
    - Cambiar margin-top de -5px a -10px
    - Mejorar alineación visual del título con el logo
    - Ajustar posición para que esté a la misma altura

--- resources/views/reportes/recibo-compra.blade.php
+++ resources/views/reportes/recibo-compra.blade.php
@@ -46,7 +46,7 @@
             color: #333;
             text-align: center;
             flex: 1;
-            margin-top: -5px;
+            margin-top: -10px;
         }
 
         .info-section {
